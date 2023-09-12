18625631746da88dcad364d6e44e466a9a57e77c
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-12 22:25:09 +0530

    conventional project completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
+
+  const { id } = useParams();
 
   useEffect(() => {
     loadUsers();
@@ -14,7 +16,10 @@ export default function Home() {
     setUsers(result.data);
   };
 
-
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${id}`);
+    loadUsers();
+  };
 
   return (
     <div className="container">
@@ -40,11 +45,18 @@ export default function Home() {
                 <td>{user.email}</td>
                 <td>
                   <button className="btn btn-primary mx-2">View</button>
-                  <Link className="btn btn-outline-primary mx-2"
-                  to={`/edituser/${user.id}`}
-                  >Edit
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
                   </Link>
-                  <button className="btn btn-danger mx-2">Delete</button>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
